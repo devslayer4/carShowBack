61ba212876b6e0e75613ae85968dcb5897cb1501
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2024-05-25 09:49:38 +0300

    feat:initialize spring projects

---
+++ src/main/java/org/carsow/carshowback/CarShowBackApplication.java
@@ -0,0 +1,13 @@
+package org.carsow.carshowback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarShowBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carsow/carshowback/CarShowBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carsow.carshowback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShowBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
